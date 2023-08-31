94b03d27874886a5956f8fa33f3c874b33c6aded
Anna <Anna@Annas-Air.attlocal.net>
2023-08-31 13:49:52 -0500

    working on studio

--- classes-part-2/exercises/src/main/java/org/launchcode/Student.java
+++ classes-part-2/exercises/src/main/java/org/launchcode/Student.java
@@ -1,5 +1,9 @@
 package org.launchcode;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 public class Student {
 
     private static int nextStudentId = 1;
@@ -7,6 +11,8 @@ public class Student {
     private int studentId;
     private int numberOfCredits = 0;
     private double gpa = 0.0;
+    private double totalQualityScore = 0.0;
+    private String gradeLevel;
 
     public Student (String name, int studentId, int numberOfCredits, double gpa) {
         this.name = name;
@@ -30,20 +36,79 @@ public class Student {
 
 
     //TODO: Uncomment and complete the getGradeLevel method here:
-//    public String getGradeLevel() {
-//        // Determine the grade level of the student based on numberOfCredits
-//    }
+    public String getGradeLevel() {
+        // Determine the grade level of the student based on numberOfCredits
+        int creditNum = this.numberOfCredits;
+
+//        HashMap<Integer, String> levelNums = new HashMap<>();
+//        levelNums.put(0, "Freshman");
+//        levelNums.put(30, "Sophomore");
+//        levelNums.put(60, "Junior");
+//        levelNums.put(90, "Senior");
+//
+//        for(Map.Entry<Integer, String> level : levelNums.entrySet()){
+//            if (creditNum > level.getKey()){
+//                this.gradeLevel = level.getValue();
+//            }
+//        }
+
+        if (creditNum < 30){
+            this.gradeLevel = "Freshman";
+        }else if (creditNum <60){
+            this.gradeLevel = "Sophomore";
+        }else if (creditNum < 90){
+            this.gradeLevel = "Junior";
+        }else {
+            this.gradeLevel = "Senior";
+        }
+        return gradeLevel;
+        //This method returns the student’s level based on the number of credits they have earned:
+        // Freshman (0-29 credits), Sophomore (30-59 credits), Junior (60-89 credits), or Senior (90+ credits).
+    }
 
     // TODO: Complete the addGrade method.
     public void addGrade(int courseCredits, double grade) {
+        //gpa = (total quality score)/(num credits)
+        Double qualityScore = courseCredits * grade;
+        totalQualityScore += qualityScore;
+        numberOfCredits += courseCredits;
+        gpa = totalQualityScore / numberOfCredits;
+
         // Update the appropriate fields: numberOfCredits, gpa
     }
 
     // TODO: Add your custom 'toString' method here. Make sure it returns a well-formatted String rather
     //  than just the class fields.
 
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", studentId=" + studentId +
+                ", numberOfCredits=" + numberOfCredits +
+                ", gpa=" + gpa +
+                ", totalQualityScore=" + totalQualityScore +
+                ", gradeLevel='" + gradeLevel + '\'' +
+                '}';
+    }
+
+
     // TODO: Add your custom 'equals' method here. Consider which fields should match in order to call two
     //  Student objects equal.
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return studentId == student.studentId && numberOfCredits == student.numberOfCredits && Double.compare(gpa, student.gpa) == 0 && Double.compare(totalQualityScore, student.totalQualityScore) == 0 && Objects.equals(name, student.name) && Objects.equals(gradeLevel, student.gradeLevel);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, studentId, numberOfCredits, gpa, totalQualityScore, gradeLevel);
+    }
 
     public String getName() {
         return name;
--- classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Menu.java
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Menu.java
@@ -1,30 +1,43 @@
 package org.launchcode;
-
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class Menu {
-    private Date lastUpdated;
+    private LocalDate lastUpdated;
     private ArrayList<MenuItem> items;
 
-    public Menu(Date d, ArrayList<MenuItem> i) {
+    public Menu(LocalDate d, ArrayList<MenuItem> i) {
         this.lastUpdated = d;
         this.items = i;
     }
 
-    public void setLastUpdated(Date lastUpdated) {
+    public void setLastUpdated(LocalDate lastUpdated) {
         this.lastUpdated = lastUpdated;
     }
-
     public void setItems(ArrayList<MenuItem> items) {
         this.items = items;
     }
-
-    public Date getLastUpdated() {
+    public LocalDate getLastUpdated() {
         return lastUpdated;
     }
-
     public ArrayList<MenuItem> getItems() {
         return items;
     }
+
+
+    public void addItem(MenuItem item){
+        items.add(item);
+    }
+
 }
+
+//Menu handles: constructing/compiling menuItems into different hashmaps (by category), date when last updated
+
+   // The menu consists of several menu items
+        // Each menu item has a price, description, and category (appetizer, main course, or dessert)
+        // It should be possible to display whether a menu item is new or not
+        //The app should know when the menu was last updated, so visitors can see that the restaurant
+        //      is constantly changing and adding exciting new items
